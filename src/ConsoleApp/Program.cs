5568c1145654d230fab3325dae859dace21ae23f
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-02-15 16:54:03 +0100

    Initial Setup

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static string Password = "123";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hi there!");
+        }
+    }
+}
